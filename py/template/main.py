5f5b7de2595bd49146e96b22ada8764f0646b5f3
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2014-03-10 18:03:31 +0000

    Add script

---
+++ py/template/main.py
@@ -0,0 +1,13 @@
+# License information goes here
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+# The line above will help with 2to3 support.
+def main():
+    print('Hello World!')
+    return 0
+#
+#
+#
+if __name__ == '__main__':
+    from sys import exit
+    exit(main())
