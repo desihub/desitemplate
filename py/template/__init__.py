0c89f619d64e1427fd356afe785a3873d816b40a
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2014-03-10 15:52:01 +0000

    Add template package

---
+++ py/template/__init__.py
@@ -0,0 +1,20 @@
+# License information goes here
+# -*- coding: utf-8 -*-
+"""
+========
+template
+========
+
+This package is a template for other DESI_ Python_ packages.
+
+A Python package should have a `version()` method that returns the
+version of the package
+
+.. _DESI: http://desi.lbl.gov
+.. _Python: http://python.org
+"""
+
+def version():
+    """Returns the version of the template package.
+    """
+    return '0.1'
