b0c05c105be2bc8e4136ddfb9cee6c5782f38d49
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2014-03-11 16:10:49 +0000

    module processing

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # License information goes here
 #
+# This setup.py file is designed to be invoked with --prefix explicitly set.
+# For example
+#
+#   python setup.py install --prefix /project/projectdirs/desi/software
+#
 # Imports
 #
 import glob
@@ -10,30 +15,22 @@ from setuptools import setup, find_packages
 #
 # Import this module to get __doc__ and version().
 #
+setup_keywords = dict()
 sys.path.insert(int(sys.path[0] == ''),'./py')
 try:
     import template
-    LONG_DESCRIPTION = template.__doc__
-    VERSION = template.version()
+    setup_keywords['long_description'] = template.__doc__
+    setup_keywords['version'] = template.version()
 except ImportError:
     #
     # Try to get the long description from the README.rst file.
     #
     if os.path.exists('README.rst'):
         with open('README.rst') as readme:
-            LONG_DESCRIPTION = readme.read()
+            setup_keywords['long_description'] = readme.read()
     else:
-        LONG_DESCRIPTION = ''
-    VERSION = '0.0.1.dev'
-#
-# Set affiliated package-specific settings.  Change these as needed.
-#
-PACKAGENAME = 'template'
-DESCRIPTION = 'DESI template package'
-AUTHOR = 'Benjamin Alan Weaver'
-AUTHOR_EMAIL = 'baweaver@lbl.gov'
-LICENSE = 'BSD'
-URL = 'https://desi.lbl.gov'
+        setup_keywords['long_description'] = ''
+    setup_keywords['version'] = '0.0.1.dev'
 #
 # Obtain svn information.
 #
@@ -60,14 +57,33 @@ def get_svn_devstr():
 #
 # Indicates if this version is a release version.
 #
-RELEASE = 'dev' not in VERSION
+RELEASE = 'dev' not in setup_keywords['version']
 if not RELEASE:
-    VERSION += get_svn_devstr()
+    setup_keywords['version'] += get_svn_devstr()
+#
+# Set general settings.  Change these as needed.
+#
+setup_keywords['name'] = 'template'
+setup_keywords['description'] = 'DESI template package'
+setup_keywords['author'] = 'Benjamin Alan Weaver'
+setup_keywords['author_email'] = 'baweaver@lbl.gov'
+setup_keywords['license'] = 'BSD'
+setup_keywords['url'] = 'https://desi.lbl.gov/svn/code/tools/template'
+#
+# Set other keywords for the setup function.  These are automated, & should
+# be left alone unless you are an expert.
 #
 # Treat everything in bin/ except *.rst as a script to be installed.
 #
-scripts = [fname for fname in glob.glob(os.path.join('bin', '*'))
-           if not os.path.basename(fname).endswith('.rst')]
+setup_keywords['scripts'] = [fname for fname in glob.glob(os.path.join('bin', '*'))
+    if not os.path.basename(fname).endswith('.rst')]
+setup_keywords['provides'] = ['template']
+setup_keywords['requires'] = ['Python (>2.6.0)']
+#setup_keywords['install_requires'] = ['Python (>2.6.0)']
+setup_keywords['zip_safe'] = False
+setup_keywords['use_2to3'] = True
+setup_keywords['packages'] = find_packages('py')
+setup_keywords['package_dir'] = {'':'py'}
 #
 # If we are using --prefix, manipulate the prefix directory and make sure
 # it is in sys.path
@@ -76,38 +92,39 @@ prefix = [arg for arg in sys.argv if arg.startswith('--prefix')]
 if len(prefix) > 0:
     i = sys.argv.index(prefix[0])
     prefdir = prefix[0].split('=')[1]
-    if os.path.basename(prefdir) == PACKAGENAME:
-        prefdir = os.path.join(prefdir,VERSION)
+    nersc_host = os.getenv('NERSC_HOST')
+    if nersc_host is not None:
+        if os.path.basename(prefdir) != nersc_host:
+            prefdir = os.path.join(prefdir,nersc_host)
+    if os.path.basename(prefdir) == setup_keywords['name']:
+        prefdir = os.path.join(prefdir,setup_keywords['version'])
     else:
-        prefdir = os.path.join(prefdir,PACKAGENAME,VERSION)
+        prefdir = os.path.join(prefdir,setup_keywords['name'],setup_keywords['version'])
     sys.argv[i] = '--prefix='+prefdir
     # print(sys.argv)
     #
     # Get the Python version
     #
-    pyversion = "python{0:d}.{1:d}".format(*sys.version_info)
-    libdir = os.path.join(prefdir,'lib',pyversion,'site-packages')
+    setup_keywords['_pyversion'] = "python{0:d}.{1:d}".format(*sys.version_info)
+    libdir = os.path.join(prefdir,'lib',setup_keywords['_pyversion'],'site-packages')
     # If os.makedirs raises an exception, we want this to halt!
     os.makedirs(libdir)
     os.environ['PYTHONPATH'] = libdir + ':' + os.environ['PYTHONPATH']
     sys.path.insert(int(sys.path[0] == ''),libdir)
+else:
+    print("Warning, you are attempting to install without setting --prefix.  This is not recommended.")
+    sys.exit(1)
+#
+# Process the module file.
+#
+module_file = os.path.join('.','etc',setup_keywords['name']+'.module')
+if os.path.exists(module_file):
+    with open(module_file) as m:
+        mod = m.read().format(**setup_keywords)
+    new_module_file = os.path.join('.','etc',setup_keywords['name']+'_'+setup_keywords['version']+'.module')
+    with open(new_module_file,'w') as m:
+        m.write(mod)
 #
 # Run setup command
 #
-setup(name=PACKAGENAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      scripts=scripts,
-      requires=['Python (>2.6.0)'],
-      #install_requires=['Python (>2.6.0)'],
-      provides=[PACKAGENAME],
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      license=LICENSE,
-      url=URL,
-      long_description=LONG_DESCRIPTION,
-      zip_safe=False,
-      use_2to3=True,
-      packages=find_packages('py'),
-      package_dir = {'':'py'}
-)
+setup(**setup_keywords)
