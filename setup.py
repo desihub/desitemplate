22df45e579218e8d3a185cbb236e6a9cb20d23b9
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2014-03-11 16:25:45 +0000

    include module processing

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@
 import glob
 import os
 import sys
+import warnings
 from setuptools import setup, find_packages
 #
 # Import this module to get __doc__ and version().
@@ -75,8 +76,13 @@ setup_keywords['url'] = 'https://desi.lbl.gov/svn/code/tools/template'
 #
 # Treat everything in bin/ except *.rst as a script to be installed.
 #
+setup_keywords['_needs_bin'] = '# '
+setup_keywords['_needs_python'] = '' # Since this is a Python package this will always be needed.
+setup_keywords['_needs_ld_lib'] = '# '
 setup_keywords['scripts'] = [fname for fname in glob.glob(os.path.join('bin', '*'))
     if not os.path.basename(fname).endswith('.rst')]
+if len(setup_keywords['scripts']) > 0:
+    setup_keywords['_needs_bin'] = ''
 setup_keywords['provides'] = ['template']
 setup_keywords['requires'] = ['Python (>2.6.0)']
 #setup_keywords['install_requires'] = ['Python (>2.6.0)']
@@ -89,7 +95,7 @@ setup_keywords['package_dir'] = {'':'py'}
 # it is in sys.path
 #
 prefix = [arg for arg in sys.argv if arg.startswith('--prefix')]
-if len(prefix) > 0:
+if len(prefix) > 0 and 'install' in sys.argv:
     i = sys.argv.index(prefix[0])
     prefdir = prefix[0].split('=')[1]
     nersc_host = os.getenv('NERSC_HOST')
@@ -111,20 +117,24 @@ if len(prefix) > 0:
     os.makedirs(libdir)
     os.environ['PYTHONPATH'] = libdir + ':' + os.environ['PYTHONPATH']
     sys.path.insert(int(sys.path[0] == ''),libdir)
+    #
+    # Process the module file.
+    #
+    module_file = os.path.join('.','etc',setup_keywords['name']+'.module')
+    if os.path.exists(module_file):
+        with open(module_file) as m:
+            mod = m.read().format(**setup_keywords)
+        new_module_file = os.path.join('.','etc',setup_keywords['name']+'_'+setup_keywords['version']+'.module')
+        with open(new_module_file,'w') as m:
+            m.write(mod)
 else:
-    print("Warning, you are attempting to install without setting --prefix.  This is not recommended.")
-    sys.exit(1)
+    if 'install' in sys.argv:
+        print("Warning, you are attempting to install without setting --prefix.  This is not recommended.")
+        sys.exit(1)
 #
-# Process the module file.
+# Run setup command.  setup() will emit warnings about unknown keywords we
+# will be passing to it, so we suppress them.
 #
-module_file = os.path.join('.','etc',setup_keywords['name']+'.module')
-if os.path.exists(module_file):
-    with open(module_file) as m:
-        mod = m.read().format(**setup_keywords)
-    new_module_file = os.path.join('.','etc',setup_keywords['name']+'_'+setup_keywords['version']+'.module')
-    with open(new_module_file,'w') as m:
-        m.write(mod)
-#
-# Run setup command
-#
-setup(**setup_keywords)
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore')
+    setup(**setup_keywords)
